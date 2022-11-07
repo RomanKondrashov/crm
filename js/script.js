b1955a124b75f1b2404b88b82905ebfcb2b10f07
“RomanKond” <kondrashovrs@gmail.com>
2022-11-07 12:20:04 +0300

    add script.js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const modalFormTitle = document.querySelector('.modal_top h2');
+const formBox = document.querySelector('.modal');
+const formCheckbox = document.querySelector('.modal__checkbox');
+const formCheckboxField = document.querySelector('.modal__checkbox-wrapper input.modal__input.modal__input_discount');
+
+console.log(modalFormTitle);
+console.log(formBox);
+console.log(formCheckbox);
+console.log(formCheckboxField);
